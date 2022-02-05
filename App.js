865e7a4ea7d4246248adb7b18ebad59d24087e8d
Sidrah Tariq <sid.tariq8@hotmail.co.uk>
2022-02-05 13:58:26 +0000

    Added bootstrap framework and some buttons

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
-const UselessTextInput = () => {
+
+
+const TextInputBox = () => {
   const [number, onChangeNumber] = React.useState(null);
 
   return (
@@ -13,7 +18,12 @@ const UselessTextInput = () => {
         placeholder="Enter..."
         keyboardType="numeric"
       />
+      <ButtonGroup style={{width:"100%"}}>
+  <Button>Like </Button>
+  <Button>Comment</Button>
+</ButtonGroup>
     </SafeAreaView>
+    
   );
 };
 
@@ -28,4 +38,7 @@ const styles = StyleSheet.create({
 
 
 
-export default UselessTextInput;
+
+
+
+export default TextInputBox;
